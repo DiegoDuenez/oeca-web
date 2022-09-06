700993cc88388f147903efddbb813799a76d6925
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-09-06 12:21:32 -0500

    init

---
+++ templates/scripts/scripts.php
@@ -0,0 +1,5 @@
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.7/dist/sweetalert2.all.min.js"></script>     
+<script src="libs/jquery/jquery.min.js"></script>
+<script src="libs/glider/glider.min.js"></script>
+<script src="libs/aos/dist/aos.js"></script>
+
