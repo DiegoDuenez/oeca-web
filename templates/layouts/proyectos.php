c57590c0684c77fb6005d194c65366380022b80e
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-09-06 15:59:23 -0500

    add: proyectos

--- templates/layouts/proyectos.php
+++ templates/layouts/proyectos.php
@@ -1,3 +1,47 @@
 <div class="proyectos">
+    
+    <div class="proyectos__header">
+        <h2 class="title">PROYECTOS <br><span>FOTOVOLTAICOS A GRAN ESCALA</span></h2>
+        
+        <div class="proyectos__descripcion">
+            <span style="color: var(--color-green); font-weight: bold;">OECA</span> Lorem ipsum dolor sit,
+            amet consectetur adipisicing elit. Eligendi nobis possimus officia,
+            minima eum maxime? Repudiandae, cumque odit? Quae laudantium sit quidem ea alias 
+            magnam quos adipisci nostrum error dignissimos.
+        </div>
+
+    </div>
+
+    <div class="proyectos__items">
+        <div class="proyectos__item">
+            <div class="proyectos__item-icon">
+                <img src="resources/img/diseño-icono.png" alt="">
+            </div>
+            <div class="proyectos__item-text">
+                DISEÑO
+            </div>
+
+            <div class="proyectos__item-icon">
+                <img src="resources/img/integracion.png" alt="">
+            </div>
+            <div class="proyectos__item-text">
+                INTEGRACIÓN
+            </div>
+
+            <div class="proyectos__item-icon">
+                <img src="resources/img/puesta.png" alt="">
+            </div>
+            <div class="proyectos__item-text">
+                PUESTA EN MARCA
+            </div>
+
+            <div class="proyectos__item-icon">
+                <img src="resources/img/asesoria.png" alt="">
+            </div>
+            <div class="proyectos__item-text">
+                ASESORÍA Y FINANCIAMIENTO
+            </div>
+        </div>
+    </div>
 
 </div>
