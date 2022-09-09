4376ea7a1330385b211c73b12e4e6f87088ee869
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-09-09 10:43:36 -0500

    fix: Fixing banner logo and card width responsive

--- templates/components/banner.php
+++ templates/components/banner.php
@@ -1,7 +1,9 @@
 <div class="banner">
 
     <div class="banner__logo">
-       <a href="./"><img src="resources/img/logo-oeca.png" alt="OECA" title="OECA"></a> 
+       <!-- <a href="./"><img src="resources/img/logo-oeca.png" alt="OECA" title="OECA"></a>  -->
+       
+       <a href="./"><img src="resources/img/logo-oeca.png" alt="OECA" title="OECA"></a>
     </div>
 
     <div class="banner__titulo" data-aos="zoom-in">
