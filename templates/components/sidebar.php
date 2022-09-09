b63a7d8cf300426eed46fb7236282950fbd91dac
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-09-09 11:38:18 -0500

    add: Add link to instagram

--- templates/components/sidebar.php
+++ templates/components/sidebar.php
@@ -1,13 +1,3 @@
-
-
-<!-- <div class="sidebar__menu--fix sidebar__menu--top-3">
-    <button type="button" class="open">
-        <i class="fa-solid fa-bars"></i>
-        MENÚ
-    </button>
-</div> -->
-
-
 <div class="sidebar sidebar--fix" id="sidebar-2-container">
 
     <div class="sidebar__menu " id="sidebar-menu-2">
@@ -17,12 +7,6 @@
        </button>
     </div>
 
-    <div class="sidebar__social-media">
-        <a href="#" title="Facebook" class="up"><i class="fa-brands fa-facebook"></i></a> 
-        <a href="#" title="Instagram" class="up"><i class="fa-brands fa-instagram"></i></a> 
-        <a href="https://api.whatsapp.com/send?phone=8717686693" target="blank" title="WhatsApp" class="up"><i class="fa-brands fa-whatsapp"></i></a> 
-        <a href="mailto:proyectos@oeca.mx" title="Correo" class="up"><i class="fa-regular fa-envelope"></i></a> 
-    </div>
 </div>
 
 
@@ -36,8 +20,8 @@
     </div>
 
     <div class="sidebar__social-media">
-        <a href="#" title="Facebook" class="up"><i class="fa-brands fa-facebook"></i></a> 
-        <a href="#" title="Instagram" class="up"><i class="fa-brands fa-instagram"></i></a> 
+        <a href="#" title="Facebook" class="up" target="blank"><i class="fa-brands fa-facebook"></i></a> 
+        <a href="https://www.instagram.com/oeca_mx/?hl=en" title="Instagram" class="up" target="blank"><i class="fa-brands fa-instagram"></i></a> 
         <a href="https://api.whatsapp.com/send?phone=8717686693" target="blank" title="WhatsApp" class="up"><i class="fa-brands fa-whatsapp"></i></a> 
         <a href="mailto:proyectos@oeca.mx" title="Correo" class="up"><i class="fa-regular fa-envelope"></i></a> 
     </div>
